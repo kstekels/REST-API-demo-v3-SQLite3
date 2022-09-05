f4ea25231c5469362502f98434c2fdd544bb1ded
Karlis <karlisstekels.dev@gmail.com>
2022-09-05 14:00:59 +0300

    initial commit

---
+++ code/create_tables.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+connection = sqlite3.connect("data.db")
+cursor = connection.cursor()
+
+create_table_users = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text)"
+cursor.execute(create_table_users)
+
+create_table_items = "CREATE TABLE IF NOT EXISTS items (name text, price real)"
+cursor.execute(create_table_items)
+
+connection.commit()
+connection.close()
